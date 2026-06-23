22a73115c1b524675f0f894538457b122c6da403
Will Holland McNeil <wmcneil@sh03-ln03.stanford.edu>
2026-06-23 16:17:19 -0700

    Make SLURM scripts portable: remove hardcoded personal paths
    - Remove hardcoded absolute paths, email, and account from all 5 SLURM scripts
    - Use cd $(dirname $0) so scripts work from any directory
    - Use SNAKEMAKE env var with snakemake as fallback
    - Replace personal details in config.cluster.yaml with placeholders
    - Update README with setup instructions for new users

--- workflow/run_slurm_ca_counterfactual2030_opt_ab32rps.sh
+++ workflow/run_slurm_ca_counterfactual2030_opt_ab32rps.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#SBATCH -A sjdavis
 #SBATCH -p serc
-#SBATCH -o /home/groups/sjdavis/will/pypsa-usa/workflow/logs/snakemake_ca_counterfactual2030_opt_ab32rps.out
-#SBATCH -e /home/groups/sjdavis/will/pypsa-usa/workflow/logs/snakemake_ca_counterfactual2030_opt_ab32rps.err
+#SBATCH -o logs/snakemake_ca_counterfactual2030_opt_ab32rps.out
+#SBATCH -e logs/snakemake_ca_counterfactual2030_opt_ab32rps.err
 #SBATCH --time 96:00:00
 #SBATCH --mail-type END,FAIL
-#SBATCH --mail-user wmcneil@stanford.edu
 
-cd /home/groups/sjdavis/will/pypsa-usa/workflow
+# cd to the workflow directory (works regardless of where sbatch is called from)
+cd "$(dirname "$0")"
+
+# Set SNAKEMAKE to the path of your snakemake executable, or ensure it is on your PATH
+SNAKEMAKE="${SNAKEMAKE:-snakemake}"
+
 export GRB_LICENSE_FILE=/share/software/user/restricted/gurobi/11.0.2/licenses/gurobi.lic
 
-/home/users/wmcneil/envs/pypsa-usa/bin/snakemake \
+$SNAKEMAKE \
   --cluster "sbatch -A {cluster.account} -p {cluster.partition} -o {cluster.output} -e {cluster.error} -c {threads} --mem {resources.mem_mb} --time {resources.walltime}" \
   --cluster-config config/config.cluster.yaml \
   --jobs 20 \
